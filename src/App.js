2f3fc72a9c494d294e7e5e23a4def52837eb4134
soyeonnn <no02031@naver.com>
2022-08-03 11:04:01 +0900

    1차 기능 구현

--- src/App.js
+++ src/App.js
@@ -12,16 +12,6 @@ export default class App extends Component {
           id: "1",
           title: "운동하기",
           completed: false
-        },
-        {
-          id: "2",
-          title: "공부하기",
-          completed: false
-        },
-        {
-          id: "3",
-          title: "밥먹기",
-          completed: false
         }
       ],
       value: ""
@@ -37,18 +27,18 @@ export default class App extends Component {
     float: "right"
   }
 
-  getStyle = () => {
+  getStyle = (completed) => {
     return {
       padding: "20px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none"
+      textDecoration: completed ? "line-through" : "none"
     }
   }
 
   deleteClick = (id) => {
     // 해당 아이디에 대한 할일 목록을 지워야 해요!
     // 데이터를 지워요. -> 열심히 하면 배열 처리 가능
-    const newTodoData = this.state.todoData.filter(data => data.id != id);
+    const newTodoData = this.state.todoData.filter(data => data.id !== id);
     // 변경된 데이터를 가지고 화면을 다시 그려야 해요!
     // 어떻게 하면 이 작업을 수행할 수 있나요?
    
@@ -64,6 +54,32 @@ export default class App extends Component {
     this.setState({value: e.target.value});
   }
 
+  handleSubmit = (e) => {
+    // 현재 submit event가 발생해서 처리하고 있어요!
+    // 이런 default event 처리를 안할래요
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),  // unique한 값을 표현하기 위해.
+      title: this.state.value,
+      completed: false
+    }
+
+    this.setState({todoData: [...this.state.todoData, newTodo], value: ""});
+  }
+
+  handleCompleteChange = (id) => {
+    // id에 대해 todoData의 completed 값을 변경시켜야 해요
+    let newTodoData = this.state.todoData.map((data) => {
+      if(data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+
+    this.setState({todoData: newTodoData});
+  }
+
   render() {
     return (
       <div className="container">
@@ -73,23 +89,25 @@ export default class App extends Component {
           </div>
 
           {this.state.todoData.map(data => (
-            <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultChecked={false} />
+            <div style={this.getStyle(data.completed)} key={data.id}>
+              <input type="checkbox" 
+                     defaultChecked={false}
+                     onChange={() => this.handleCompleteChange(data.id)} />
               {data.title}
               <button style={this.btnStyle} onClick={() => this.deleteClick(data.id)}>delete</button>
             </div>
           ))}
 
-          <form style={{ display: 'flex' }}>
-            <input type="text" 
-                   name="todoInput" 
+          <form style={{ display: 'flex' }} onSubmit={this.handleSubmit}>
+            <input type="text"
+                   name="todoInput"
                    style={{ flex: '10', padding: '5px' }} 
-                   placeholder="새로운 할 일을 입력하세요" 
+                   placeholder="새로운 할 일을 입력하세요"
                    value={this.state.value}
                    onChange={this.handleChange} />
-            <input type="submit" 
-                   value="입력" 
-                   className="btn" 
+            <input type="submit"
+                   value="입력"
+                   className="btn"
                    style={{ flex: '1' }} />
           </form>
         </div>
